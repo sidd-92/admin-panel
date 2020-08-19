27523b18d3d7b6c36d41e1bdc6e273b97000ddfd
Siddharth Natrajan <sidddharthvn2014@gmail.com>
2020-08-19 15:40:59 +0530

    Login

--- client/src/components/views/Login/Login.js
+++ client/src/components/views/Login/Login.js
@@ -1,37 +1,82 @@
 import React, { Component } from "react";
 
 import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
+import TextField from "@material-ui/core/TextField";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import Visibility from "@material-ui/icons/Visibility";
+import Input from "@material-ui/core/Input";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+import InputLabel from "@material-ui/core/InputLabel";
+import FormControl from "@material-ui/core/FormControl";
+import OutlinedInput from "@material-ui/core/OutlinedInput";
 import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-import { Container } from "@material-ui/core";
+
+import IconButton from "@material-ui/core/IconButton";
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showPassword: false,
+      password: "",
+    };
+  }
   render() {
     return (
-      <Container maxWidth="sm">
-        <h2>Login</h2>
-        <Card>
+      <div className="w-full flex justify-center items-center h-screen p-2 sm:p-12">
+        <Card className="w-full sm:w-1/3 h-full px-0 py-0 sm:px-4 sm:py-8">
           <CardContent>
-            <Typography color="textSecondary" gutterBottom>
-              Word of the Day
-            </Typography>
-            <Typography variant="h5" component="h2">
-              benevolent
-            </Typography>
-            <Typography color="textSecondary">adjective</Typography>
-            <Typography variant="body2" component="p">
-              well meaning and kindly.
-              <br />
-              {'"a benevolent smile"'}
-            </Typography>
+            <div className="text-2xl font-bold">Login</div>
+            <div className="text-lg font-light">Welcome To Admin Panel</div>
+            <form noValidate autoComplete="off">
+              <div className="pt-6">
+                <TextField
+                  id="outlined-basic"
+                  label="Outlined"
+                  variant="outlined"
+                  fullWidth={true}
+                />
+              </div>
+              <div className="pt-6">
+                <FormControl fullWidth variant="outlined">
+                  <InputLabel htmlFor="outlined-adornment-password">
+                    Password
+                  </InputLabel>
+                  <OutlinedInput
+                    id="outlined-adornment-password"
+                    type={this.state.showPassword ? "text" : "password"}
+                    value={this.state.password}
+                    onChange={(e) =>
+                      this.setState({ password: e.target.value })
+                    }
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={() =>
+                            this.setState({
+                              showPassword: !this.state.showPassword,
+                            })
+                          }
+                          edge="end"
+                        >
+                          {this.state.showPassword ? (
+                            <Visibility />
+                          ) : (
+                            <VisibilityOff />
+                          )}
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                    labelWidth={70}
+                  />
+                </FormControl>
+              </div>
+            </form>
           </CardContent>
-          <CardActions>
-            <Button size="small">Learn More</Button>
-          </CardActions>
         </Card>
-      </Container>
+      </div>
     );
   }
 }
